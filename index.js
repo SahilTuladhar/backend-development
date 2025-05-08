4665bb74d8459eed78edf6d81e93d4864c640a44
SailTuladhar <sailratna11@gmail.com>
2025-05-08 17:19:23 +0545

    familiar with express

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT ? process.env.PORT : 3000
+ 
+const userData = [ {
+    name: 'Messi',
+    age: 38,
+    role: 'CAM'
+} , {
+    name: 'Saka',
+    age: 22,
+    role: 'RW'
+}]
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,6 +23,11 @@ app.get('/twitter' , (req , res) => {
  res.send('twitter username: sailTuladhar')
 })
 
+app.get('/footballers' , (req , res)=>{
+ res.json(userData)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
