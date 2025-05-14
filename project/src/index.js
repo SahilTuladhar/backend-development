0dc145221b8a30bc70fcd2cc4dbe7e836f3f10bf
SailTuladhar <sailratna11@gmail.com>
2025-05-14 17:06:28 +0545

    express instance created

--- project/src/index.js
+++ project/src/index.js
@@ -5,7 +5,28 @@ dotenv.config({
 
 
 import connectDB from "./db/index.js";
+import app from './app.js';
+import { error } from 'console';
 
 
 
 connectDB()
+.then(() => {
+
+app.on('error' , (err) => {
+
+    console.log('ERR:' , err);
+    throw err
+    
+
+})
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Server sucessfully running at port ${process.env.PORT}`);
+    
+})
+})
+.catch((err) => {
+    console.log("MongoDB connection Failed :" , err);
+    
+})
