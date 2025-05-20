836f82e8e4782790112a8692ddc4f04dca38fd8f
SailTuladhar <sailratna11@gmail.com>
2025-05-20 11:44:56 +0545

    cloudinary logic added and multer concepts identified

--- project/src/utils/Cloudinary.js
+++ project/src/utils/Cloudinary.js
@@ -1,10 +1,32 @@
-import { v2 as cloudinary } from 'cloudinary';
-import fs from 'fs'
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
 
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME ,
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET 
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
     });
+
+    console.log('File has been successfully uploaded and its url is:' , response.url);
+    
+
+    fs.unlinkSync(localFilePath); // unlinking after successful uoload to the cloud
+    return response;
+  } catch (err) {
+    console.log(err);
+    fs.unlinkSync(localFilePath); //unlink the file if it leads to error
+    return null;
+  }
+};
+
+export default uploadOnCloudinary;
